86ca9a72fcf795e74f7ced8ca107b90747c5c04a
ivanbonilla <ivanbonilla@criptext.com>
2023-09-19 00:24:50 -0500

    primer commit

---
+++ lab17.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset='utf-8'>
+        <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+        <title>laboratorio 1.7</title>
+        <meta name='viewport' content='width=device-width, initial-scale=1'>
+        <link rel='stylesheet' type='text/css' media='screen' href='main.css'>
+        <script src='main.js'></script>
+    </head>
+    <body>
+        <?php 
+            $x = 24;
+            $pi = 3.1416;
+            $animal = "conejo";
+            $saludo = "hola caracola";
+            echo $x, "<br>", $pi, "<br>", $animal, "<br>", $saludo;
+        ?>
+    </body>
+</html>
